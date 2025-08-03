c51c650d5bed60bdd75115f424ab7e5a142f96a4
Guillermo Reyes <reyesguillermo54@yahoo.com>
2025-08-03 08:47:44 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Guillermo Reyes on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct CardView: View {
+   @State var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("😭").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Guillermo Reyes on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
