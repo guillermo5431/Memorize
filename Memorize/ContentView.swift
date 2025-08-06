fde85044087f34798e8f0b1f2bbdc6780fb6646b
Guillermo Reyes <reyesguillermo54@yahoo.com>
2025-08-06 12:16:37 -0500

    Added the For Each loop and new let variable

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["😂", "😭", "😒", "😏"]
+    
     var body: some View {
         HStack {
-            CardView()
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self){ index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -24,6 +26,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
    @State var isFaceUp = false
     
     var body: some View {
@@ -32,7 +35,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("😭").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
